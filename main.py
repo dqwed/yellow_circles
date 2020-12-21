df3fb2a880e03085d91434a12de783c34899fbe1
dqwed <m.dan3k@yandex.ru>
2020-12-21 15:52:51 +0300

    colored circles

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import random
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QPen, QBrush
+from PyQt5.QtGui import QPainter, QPen, QBrush, QColor
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
@@ -21,17 +21,24 @@ class DrawYellowCircles(QWidget):
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        self.draw_circle(qp)
+        self.draw_circles(qp)
         qp.end()
 
-    def draw_circle(self, qp):
+    def draw_circles(self, qp):
         number = random.randint(1, 10)
-        qp.setPen(QPen(Qt.yellow))
-        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
         for i in range(number):
+            r, g, b = self.select_color()
+            qp.setPen(QPen(QColor(r, g, b)))
+            qp.setBrush(QBrush(QColor(r, g, b), Qt.SolidPattern))
             diameter = random.randint(10, 200)
             qp.drawEllipse(random.randint(0, 800 - diameter), random.randint(0, 800 - diameter),
                            diameter, diameter)
+
+    def select_color(self):
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        return red, green, blue
 
 
 if __name__ == '__main__':
